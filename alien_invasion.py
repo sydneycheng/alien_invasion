a20397e069a16459ecf1fadff055b1357f3aded2
Sydney Cheng <sydney_cheng1@baylor.edu>
2021-10-20 15:58:14 -0500

    commit 2

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,12 +6,10 @@ from ship import Ship
 #Create class AlienInvasion
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
-
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))  #we've created an insance of Settings
                                                                         #& assigned it to self.settings
@@ -22,7 +20,6 @@ class AlienInvasion:
                             #the surface returned by display.set_mode() represents the entire game window
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-
         self.ship = Ship(self)  #this is the paramater that gives Ship access to the game's resources, like the screen object
 
         # Set the background color. Light Gray
@@ -35,7 +32,7 @@ class AlienInvasion:
             self._check_events() #call this method from within the Class
             self._update_screen()
         
-
+        
     def _check_events(self):
         """Respond to keypresses and mouse events."""
                 # Watch for keyboard and mouse events.
